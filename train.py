4283b29c6d49df48f2ef330df4299e81514f4401
Kuzmarg <vovakuz45616@gmail.com>
2024-05-12 00:28:46 +0300

    log time inclusion

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import torch
 import os
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 def train(config: dict):
